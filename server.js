c2d8ed637083c4671f8344ba9c95e04f33d36e52
Quv0nchbek <nuriddinovquvonchbek4903@gmail.com>
2025-01-08 21:49:25 +0500

    Express qo'shish

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const server = express();4
+const PORT = 9321;
+
+server.listen(PORT, () => console.log(`SERVER PORT IS ${PORT}`))
+
+server.get("/", (req, res) => {
+    res.send({
+        ok: true,
+        code: 200
+
+    })
+})
