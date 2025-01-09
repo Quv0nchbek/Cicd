f41986453bbe14bc8154f56f20f2958e116fb6ab
Quv0nchbek <nuriddinovquvonchbek4903@gmail.com>
2025-01-09 15:13:59 +0500

    Salom dunyo

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ server.listen(PORT, () => console.log(`SERVER PORT IS ${PORT}`))
 server.get("/", (req, res) => {
     res.send({
         ok: true,
-        code: 200
+        code: 200,
+        message: "salom dunyo"
 
     })
 })
